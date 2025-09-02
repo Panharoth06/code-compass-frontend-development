6603f0946fde4c608f3d918b2060857dde7f2f6c
Panharoth06 <panharothchheng0903@gmail.com>
2025-09-03 04:11:14 +0700

    merged branch Raksmey

--- src/app/about-us/page.tsx
+++
@@ -1,8 +0,0 @@
-import Aboutus from '@/components/Aboutus'
-import React from 'react'
-
-export default function page() {
-  return (
-    <div><Aboutus/></div>
-  )
-}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import Aboutus from '@/components/Aboutus'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div><Aboutus/></div>
+  )
+}
