1aca1d9ae482a38d8a71c1db549888a61fe894f4
Hor Ratha <you@example.com>
2025-09-13 07:49:42 +0700

    push creator dashboard with simple version, not yet use text editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Homepage from "@/components/homePage/HomePage";
-
-
+import CreatorDashboard from "@/components/creator_Dashboard/creatorDashboard";
 
 export default function Home() {
   return (
     <div>
 
       <Homepage />
+      <CreatorDashboard/>
   
 
     </div>
