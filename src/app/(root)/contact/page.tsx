2568e6e02e4e406a63f056a4084a4d76a0cb324d
Panharoth06 <panharothchheng0903@gmail.com>
2025-09-15 15:43:18 +0700

    feat: add Contact Us page with form handling and dark mode support

--- src/app/(root)/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContactUsPage from "@/components/contact_Us/contactUs";
-
-export default function Contact() {
-  return (
-    <div>
-      <ContactUsPage />
-    </div>
-  );
-}
--- src/app/(root)/leaderboard/page.tsx
+++ src/app/(root)/leaderboard/page.tsx
@@ -1,5 +1,14 @@
-import ModernCodeCompassLeaderboard from "@/components/leaderboard/leaderboard";
+'use client'
 
-export default function page() {
-  return <ModernCodeCompassLeaderboard/>;
+import React from 'react'
+import ModernCodeCompassLeaderboard from '@/components/leaderboard/ModernCodeCompassLeaderboard'
+
+const ProblemsetPage: React.FC = () => {
+  return (
+    <div className="w-full">
+      <ModernCodeCompassLeaderboard />
+    </div>
+  )
 }
+
+export default ProblemsetPage
