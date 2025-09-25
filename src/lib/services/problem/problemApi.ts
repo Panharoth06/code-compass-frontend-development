14a3252aeaa0bff2617182519dd5c1e00957e470
chengdevith <chengdevith5@gmail.com>
2025-09-25 16:30:35 +0700

    feat(cmt): cmt

--- src/lib/services/problem/problemApi.ts
+++ src/lib/services/problem/problemApi.ts
@@ -5,7 +5,7 @@ export const problemApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getProblem: builder.query<ProblemResponse, number>({
       query: (id) => {
-        return { url: `problems/${id}/me`, method: "GET" };
+        return { url: `problems/${id}`, method: "GET" };
       },
       providesTags: (result, error, id) => [{ type: "Problems", id }],
     }),
